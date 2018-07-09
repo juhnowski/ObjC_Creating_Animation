d5ef48fd19175940ba0d5409e86058e157153ac8
docophila <docophila@gmail.com>
2018-07-09 20:00:32 +0300

    first commit

--- ObjC_Creating_Animation/ViewController.h
+++ ObjC_Creating_Animation/ViewController.h
@@ -9,6 +9,9 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *ImageView1;
+@property (weak, nonatomic) IBOutlet UIImageView *ImageView2;
+- (IBAction)PlayAnimation:(id)sender;
 
 
 @end
